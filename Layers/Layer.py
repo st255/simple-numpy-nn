204a0cd44a03fcc124683b881dbf861fdc3cd8bc
st255 <lucasmrtfdz@gmail.com>
2026-06-14 17:08:19 +0200

    Added training param

--- Layers/Layer.py
+++ Layers/Layer.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Layer(ABC):
     @abstractmethod
-    def forward(self, inputs):
+    def forward(self, inputs, training: bool = True):
         pass
 
     @abstractmethod
